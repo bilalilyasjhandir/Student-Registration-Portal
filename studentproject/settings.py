e46762af8da090299d334054ccd829dc5b6a4da5
bilalilyasjhandir <bilaljhandir007@gmail.com>
2025-04-10 21:01:18 +0500

    setup environment for deployment

--- studentproject/settings.py
+++ studentproject/settings.py
@@ -11,6 +11,9 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
+import os
+import dj_database_url
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -73,16 +76,15 @@ WSGI_APPLICATION = 'studentproject.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'studentdb',
-        'USER': 'postgres',
-        'PASSWORD': '1234',
-        'HOST': 'localhost',
-        'PORT': '5432',
-    }
+    'default': dj_database_url.config(
+        default='postgres://postgres:1234@localhost:5432/studentdb',
+        conn_max_age=600
+    )
 }
+
+ALLOWED_HOSTS = ['*'] 
 
 
 # Password validation
